1f85ba171f32bbd884c95eb5292f990f1fd5d926
Juan David Mendez Campuzano <juandavidmendezcampuzano@gmail.com>
2022-04-26 21:47:04 -0500

    cambios en calendar

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from "react";
+import { Navbar } from "../ui/Navbar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import { messages } from "../../helpers/calendar-messages-es";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "moment/locale/es";
+import { CalendarEvent } from "./CalendarEvent";
+
+moment.locale("es");
+
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+const myEventsList = [
+  {
+    title: "Cumple de alguien",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+    notes:'Comprar la torta',
+    user: {
+      _id: '123',
+      name:'Juan'
+    }
+  },
+];
+
+
 
 export const CalendarScreen = () => {
+
+  const eventStartGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+    return {
+      style,
+    };
+  };
+
   return (
-    <div>Calendar Screen</div>
-  )
-}
+    <div className="calendar-screen">
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStartGetter}
+        components={{
+          event: CalendarEvent,
+        }}
+      />
+    </div>
+  );
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate} from "react-router-dom";
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 
